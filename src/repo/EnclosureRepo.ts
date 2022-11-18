66b799dd44c995eda79a8ecfcd5fd055f555f4d2
LLL762 <lamirallaurent@gmail.com>
2022-11-18 11:00:35 +0100

    cleaning

--- src/repo/EnclosureRepo.ts
+++ src/repo/EnclosureRepo.ts
@@ -1,32 +1,25 @@
 import { Types } from "mongoose";
 import Enclosure from "../model/Enclosure";
 
+const projectionMin = { name: 1, _id: 1, gpsCoordinates: 1 };
+
 const findByZoneId = async (
   zoneId: string,
   pageIndex: number,
   pageSize: number
 ) => {
-  const skip = (pageIndex - 1) * pageSize;
-
   return Enclosure.m
     .aggregate()
     .match({ zone: new Types.ObjectId(zoneId) })
     .facet({
       page: [{ $count: "count" }],
       enclosures: [
-        { $skip: skip },
+        { $skip: (pageIndex - 1) * pageSize },
         { $limit: pageSize },
         { $sort: { name: 1 } },
-        { $project: { name: 1, _id: 1, gpsCoordinates: 1 } },
+        { $project: projectionMin },
       ],
     });
 };
-
-/*   Enclosure.m
-    .find({ zone: zoneId }, { _id: 1, name: 1, gpsCoordinates: 1 })
-    .skip((pageIndex - 1) * pageSize)
-    .limit(pageSize)
-    .sort({ name: 1 })
-    .exec(); */
 
 export default { findByZoneId };
